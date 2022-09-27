9beb53c13c56b01e09977831d7362bc8ff44937b
Genaro Garcia <genaro.dev@outlook.com>
2022-09-27 12:37:16 -0500

    playRound function created

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const choices = ['rock', 'paper', 'scissors'];
 function getComputerChoice() {
   const choice = choices[Math.floor(Math.random() * choices.length)];
   return choice;
-  console.log(choice);
 }
-console.log(getComputerChoice(choices));
+
+function playRound(playerSelection, computerSelection) {
+  computerSelection = getComputerChoice();
+  const win = `You Win! ${playerSelection} beats ${computerSelection}`;
+
+  if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return win;
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return win;
+  } else if (playerSelection == 'scissors' && computerSelction == `paper`) {
+    return win;
+  } else if (playerSelection == computerSelection) {
+    return "It's a tie!";
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const playerChoice = 'rock';
+console.log(playRound(playerChoice, getComputerChoice()));
