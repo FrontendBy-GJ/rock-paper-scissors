65b562f944a509e9cf6f1486cfa1132dcc03e6e3
Genaro Garcia <genaro.dev@outlook.com>
2022-09-28 11:44:06 -0500

    Finished
    New functions: checkWinner, playerChoice function accepts case sensitive
    user input and game function, play 5 round game and keeps score.

--- app.js
+++ app.js
@@ -5,22 +5,73 @@ function getComputerChoice() {
   return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
-  computerSelection = getComputerChoice();
-  const win = `You Win! ${playerSelection} beats ${computerSelection}`;
-
-  if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    return win;
-  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-    return win;
-  } else if (playerSelection == 'scissors' && computerSelction == `paper`) {
-    return win;
-  } else if (playerSelection == computerSelection) {
-    return "It's a tie!";
-  } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+function playerChoice() {
+  let valid = false;
+  while (valid == false) {
+    const choice = prompt('Rock Paper or Scissors');
+    if (choice == null || '') {
+      continue;
+    }
+    const choiceLwrCase = choice.toLowerCase();
+    if (choices.includes(choiceLwrCase)) {
+      valid = true;
+      return choiceLwrCase;
+    }
   }
 }
 
-const playerChoice = 'rock';
-console.log(playRound(playerChoice, getComputerChoice()));
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return 'You Win!';
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return 'You Win!';
+  } else if (playerSelection == 'scissors' && computerSelection == `paper`) {
+    return 'You Win!';
+  } else if (playerSelection == computerSelection) {
+    return 'Tie!';
+  } else {
+    return 'You Lose!';
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  const win_or_lose = checkWinner(playerSelection, computerSelection);
+
+  if (win_or_lose == 'You Win!') {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if (win_or_lose == 'You Lose!') {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return "It's a Tie!";
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = playerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(`Round ${i + 1}`);
+    console.log(playRound(playerSelection, computerSelection));
+
+    if (checkWinner(playerSelection, computerSelection) == 'You Win!') {
+      playerScore++;
+    } else if (checkWinner(playerSelection, computerSelection) == 'You Lose!') {
+      computerScore++;
+    }
+  }
+  console.log('Game Over!');
+  if (playerScore > computerScore) {
+    console.log('Player Wins!');
+  } else if (playerScore < computerScore) {
+    console.log('Computer Wins!');
+  } else {
+    console.log(`It's a tie!`);
+  }
+  console.log(
+    `Final Results\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`
+  );
+}
+game();
